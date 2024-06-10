d61627d77f9c91d9e86542ff390082cbc781c6d9
gui-iha <guilherme.hideki.iha@gmail.com>
2024-06-10 20:57:14 -0300

    atualização pagina trabalho 2 web

--- scripts/main.js
+++ scripts/main.js
@@ -21,23 +21,64 @@ function changeAll() {
     var b = document.getElementById("valBlue").innerHTML;
     document.getElementById("colorchanger").style.backgroundColor =
         "rgb(" + r + "," + g + "," + b + ")";
+    document.getElementById("gallerycolorchanger").style.backgroundColor =
+        "rgb(" + r + "," + g + "," + b + ")";
     document.getElementById("footercolorchanger").style.backgroundColor =
         "rgb(" + r + "," + g + "," + b + ")";
-    document.getElementById("footer").style.backgroundColor =
-        "rgb(" + r + "," + g + "," + b + ")";
+
 }
-function changetrends() {
-    var r = document.getElementById("valRed").innerHTML;
-    var g = document.getElementById("valGreen").innerHTML;
-    var b = document.getElementById("valBlue").innerHTML;
-    document.getElementById("colorchanger").style.backgroundColor =
-        "rgb(" + r + "," + g + "," + b + ")";
+
+function changetrends(value) {
+
+    var galleryRvalue,galleryGvalue,galleryBvalue;
+    var footerRvalue, footerGvalue,footerBvalue;
+    if(value == 2050){
+        galleryRvalue = 0;
+        galleryGvalue = 255;
+        galleryBvalue = 159;
+        footerRvalue = 214;
+        footerGvalue =  0;
+        footerBvalue = 255;
+    }else if(value == 2024){
+        galleryRvalue = 254;
+        galleryGvalue = 216;
+        galleryBvalue = 177;
+        footerRvalue = 111;
+        footerGvalue =  78;
+        footerBvalue = 55;
+    }else if(value == 2020){
+        galleryRvalue = 65;
+        galleryGvalue = 244;
+        galleryBvalue = 248;
+        footerRvalue = 82;
+        footerGvalue =  193;
+        footerBvalue = 190;
+    }else if(value == 2015){
+        galleryRvalue = 171;
+        galleryGvalue = 94;
+        galleryBvalue = 95;
+        footerRvalue = 32;
+        footerGvalue =  66;
+        footerBvalue = 83;
+    }else if(value == 2010){
+        galleryRvalue = 208;
+        galleryGvalue = 93;
+        galleryBvalue = 40;
+        footerRvalue = 40;
+        footerGvalue =  156;
+        footerBvalue = 210;
+    }else if(value == 2000){
+        galleryRvalue = 166;
+        galleryGvalue = 192;
+        galleryBvalue = 213;
+        footerRvalue = 127;
+        footerGvalue =  147;
+        footerBvalue = 147;
+    }
+    document.getElementById("gallerycolorchanger").style.backgroundColor =
+        "rgb(" + galleryRvalue + "," + galleryGvalue + "," + galleryBvalue + ")";
     document.getElementById("footercolorchanger").style.backgroundColor =
-        "rgb(" + r + "," + g + "," + b + ")";
-    document.getElementById("footer").style.backgroundColor =
-        "rgb(" + r + "," + g + "," + b + ")";
-    document.getElementById("slidescolorchanger").style.backgroundColor =
-        "rgb(" + r + "," + g + "," + b + ")";
+        "rgb(" + footerRvalue + "," + footerGvalue + "," + footerBvalue + ")";
 }
 
 let slideIndex = 1;
